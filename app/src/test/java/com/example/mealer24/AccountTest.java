c2ccb447bb68754384e45ffad0e7066c9388396d
Alexandre Ringuette <aring057@uottawa.ca>
2022-11-17 18:33:07 -0500

    updated Account unit test

--- app/src/test/java/com/example/mealer24/AccountTest.java
+++ app/src/test/java/com/example/mealer24/AccountTest.java
@@ -2,27 +2,26 @@ package com.example.mealer24;
 
 import static org.junit.Assert.assertNotEquals;
 
-import junit.framework.TestCase;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
-public class AccountTest extends TestCase {
+public class AccountTest {
 
-    @Rule
     Account account = new Account("email@email.com", "pass", "nom", "nomFamille", "address");
 
     @Test
     public void testGetSetEmail() {
         account.setEmail("anotherEmail@gmail.com");
-        assertEquals("anotherEmail@gmail.com", account.getEmail());
+        Assert.assertEquals("anotherEmail@gmail.com", account.getEmail());
         assertNotEquals("email@email.com", account.getEmail());
     }
 
     @Test
     public void testGetSetPwd() {
         account.setPwd("differentPass");
-        assertEquals("differentPass", account.getPwd());
+        Assert.assertEquals("differentPass", account.getPwd());
         assertNotEquals("pass", account.getPwd());
 
     }
@@ -30,30 +29,30 @@ public class AccountTest extends TestCase {
     @Test
     public void testGetSetNom() {
         account.setNom("Alex");
-        assertEquals("Alex", account.getNom());
+        Assert.assertEquals("Alex", account.getNom());
         assertNotEquals("nom", account.getNom());
     }
 
     @Test
     public void testGetSetNomFamille() {
         account.setNomDeFamille("Ringuette");
-        assertEquals("Ringuette", account.getNomFamille());
+        Assert.assertEquals("Ringuette", account.getNomFamille());
         assertNotEquals("nomFamille", account.getNomFamille());
     }
 
     @Test
     public void testGetSetAddress() {
         account.setAddress("differentAddress");
-        assertEquals("differentAddress", account.getAddress());
+        Assert.assertEquals("differentAddress", account.getAddress());
         assertNotEquals("address", account.getAddress());
 
     }
 
     @Test
     public void testGetSetLogInStatus() {
-        assertFalse(account.getLogInStatus());
+        Assert.assertFalse(account.getLogInStatus());
         account.setLogInStatus(true);
-        assertTrue(account.getLogInStatus());
+        Assert.assertTrue(account.getLogInStatus());
     }
 
 
