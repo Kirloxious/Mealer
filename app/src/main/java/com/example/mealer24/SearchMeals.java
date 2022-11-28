c1b4e86d59f9fc790d9323fcaf02acd2d54cfa58
KheswariG <kgukh078@uottawa.ca>
2022-11-28 14:02:04 -0500

    just added a simple activity

---
+++ app/src/main/java/com/example/mealer24/SearchMeals.java
@@ -0,0 +1,14 @@
+package com.example.mealer24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchMeals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_meals);
+    }
+}
