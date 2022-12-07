dc29e336f1e77d9f9b19b33d6a2b83d183053aaa
Utku Cerit <utct@protonmail.com>
2022-12-07 01:57:29 -0500

    added one more test

---
+++ app/src/test/java/com/example/mealer24/DemandeAchatTest.java
@@ -0,0 +1,39 @@
+package com.example.mealer24;
+
+import junit.framework.TestCase;
+
+public class DemandeAchatTest extends TestCase {
+
+    public void testGetOrderId() {
+    }
+
+    public void testSetOrderId() {
+    }
+
+    public void testGetMealId() {
+    }
+
+    public void testSetMealId() {
+    }
+
+    public void testGetCookEmail() {
+    }
+
+    public void testSetCookEmail() {
+    }
+
+    public void testGetClientEmail() {
+    }
+
+    public void testSetClientEmail() {
+    }
+
+    public void testGetOrderStatus() {
+    }
+
+    public void testSetOrderStatus() {
+    }
+
+    public void testToMap() {
+    }
+}
